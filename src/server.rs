9d7d5eaed9359cb54bd123ab8e85975ee2fe4218
Stanislav Ursache <stanislavursache@outlook.com>
2017-12-04 00:48:20 +0300

    ls works?

--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,18 @@
 
+use std::path::PathBuf;
+
 #[get("/ls")]
 pub fn ls_empty() -> String {
     ::adb::ls("/")
 }
 
-#[get("/ls/<p>")]
-pub fn ls(p: String) -> String {
-    ::adb::ls(&p)
+#[get("/ls/<p..>")]
+pub fn ls(p: PathBuf) -> String {
+    let mut n = p.to_str().expect("ex").to_string();
+
+    n = n.replace("\\", "/");
+
+    println!("ls -> {}", &n);
+
+    ::adb::ls(&n)
 }
